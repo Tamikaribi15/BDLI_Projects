f00c4477cea60cb57c5989723252e4c1cecd4996
manueljo <mikebillyy@gmail.com>
2024-09-09 23:27:43 +0100

    employee update task status

--- public/assets/js/employee.js
+++ public/assets/js/employee.js
@@ -1,12 +1,16 @@
+
+// =============== function for allowing drop
 function allowDrop(event) {
   event.preventDefault();
 }
 
+// ================= function for item drag
 function drag(event) {
   event.dataTransfer.setData("text", event.target.id);
   event.target.classList.add('dragging');
 }
 
+// =============== function for item drop
 function drop(event, status) {
   event.preventDefault();
   event.target.style.minHeight = '';
@@ -18,8 +22,20 @@ function drop(event, status) {
 
   if (element) {
     element.classList.remove('dragging');
-    // Change status based on the drop target
+    // ======= change status
     element.querySelector("p#status").innerText = status;
+
+    // Get values from the dragged element
+    const taskId = element.querySelector('p#taskId').innerText;
+
+    // Populate hidden form fields
+    document.getElementById('formStatus').value = status;
+    document.getElementById('formId').value = taskId;
+
+    // Submit the form to reload the page
+    document.getElementById('taskForm').submit();
+
+
 
     // Move the element to the new section
     event.target.appendChild(element);
@@ -30,23 +46,23 @@ function drop(event, status) {
 
 // ===================== testing drag effect ====================
 function resetEffect(event) {
-    var data = event.dataTransfer.getData("text");
-    var element = document.getElementById(data);
-    if (element) {
-        element.classList.remove('dragging');
-    }
+  let data = event.dataTransfer.getData("text");
+  let element = document.getElementById(data);
+  if (element) {
+    element.classList.remove('dragging');
+  }
 }
 
- // notification group and haamburger group should not display together
- const notification = document.querySelector('.notification');
- const dropdown = document.querySelector('.dropdown');
- notification.addEventListener('change', function () {
-     if (this.checked) {
-         dropdown.checked = false;
-     } 
- });
- dropdown.addEventListener('change', function () {
-     if (this.checked) {
-         notification.checked = false;
-     } 
- });
+// notification group and haamburger group should not display together
+const notification = document.querySelector('.notification');
+const dropdown = document.querySelector('.dropdown');
+notification.addEventListener('change', function () {
+  if (this.checked) {
+    dropdown.checked = false;
+  }
+});
+dropdown.addEventListener('change', function () {
+  if (this.checked) {
+    notification.checked = false;
+  }
+});
--- routes/employee.js
+++ routes/employee.js
@@ -17,7 +17,31 @@ router.get('/dashboard', isAuthenticated, async (req, res) => {
         const pendingTasks = tasks.filter(task => task.status === 'Pending');
         const completedTasks = tasks.filter(task => task.status === 'Completed');
 
-        return res.render('employee_dashboard', { pendingTasks: pendingTasks, completedTasks:completedTasks });
+        return res.render('employee_dashboard', { pendingTasks: pendingTasks, completedTasks: completedTasks });
+
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+router.post('/dashboard', isAuthenticated, async (req, res) => {
+    
+    try {
+        const { taskId, status } = req.body;
+
+        const task = await Task.findByPk(taskId);
+
+        // ======If the task is not found, return a 404 error
+        if (!task) {
+            return res.status(404).json({ error: 'Task not found' });
+        }
+
+        // Update the task fields with the new data
+        task.status = status || task.status;
+
+        // ======== Save the updated task back to the database
+        await task.save();
+        return res.redirect('/employee/dashboard');
 
     } catch (error) {
         console.error(error);
